ee1fda9e2eaacf820cd6ea45408d66fe385df72d
HelsinkiAxv <helsinkiaxv@gmail.com>
2025-03-23 21:04:13 +0530

    Update

--- 1_Array/Basic.cpp
+++ 1_Array/Basic.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a[5] = {1,2,3,4,5};
-    int n = sizeof(a)/sizeof(int);
+int main() {
 
-    cout << n << endl;
+	int a[] = {1, 2, 3, 4, 5}; // Initialisation: Array ko banante time value dena
+	// int a[10] = {1, 2, 3, 4, 5}; // Initialisation: Array ko banante time value dena
+	int n = sizeof(a) / sizeof(int);
 
-    for (int i = 0; i<n; i++){
-        cout << a[i] <<" ";
-    }
-    cout << endl;
-    int x = 4;
-    int i;
-    for(i = 0; i < n; i++){
-        if(x == a[i]){
-            cout<<"True"<<endl;
-            break;
-        }
-    }
-    if(i == n){
-        cout << "False" << endl;
-    }
+	for (int i = 0; i < n; ++i)
+	{
+		cout << a[i] << " ";
+	}
 
-    cout << endl;
-    return 0;
+	cout << endl;
+
+	return 0;
 }
