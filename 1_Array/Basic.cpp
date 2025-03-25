b717bfa0f3e1daa4204d38f006b1e13d965312ec
HelsinkiAxv <helsinkiaxv@gmail.com>
2025-03-26 04:14:39 +0530

    Update

--- 1_Array/Basic.cpp
+++ 1_Array/Basic.cpp
@@ -7,8 +7,7 @@ int main() {
 	// int a[10] = {1, 2, 3, 4, 5}; // Initialisation: Array ko banante time value dena
 	int n = sizeof(a) / sizeof(int);
 
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i) {
 		cout << a[i] << " ";
 	}
 
